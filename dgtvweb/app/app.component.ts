54a5b7c99252e444db027cb8ceae1e8fbf96aa03
David Giard <djgiard@hotmail.com>
2016-08-11 23:27:27 -0500

    Add bootstrap stylings

--- dgtvweb/app/app.component.ts
+++ dgtvweb/app/app.component.ts
@@ -8,8 +8,8 @@ import { TfHeaderComponent } from './tfHeader.component';
 @Component({
     selector: 'my-app',
     template: `
-      <tfHeader [title]='title' [subTitle]='subTitle'></tfHeader>
-  <router-outlet></router-outlet>
+      <tfHeader [title]='title' [subTitle]='subTitle' class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></tfHeader>
+      <router-outlet></router-outlet>
     `,
   directives: [ROUTER_DIRECTIVES, TfHeaderComponent],
     providers: [
