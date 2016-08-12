68e53f81eeed8ff215d968ab46495d42ee88ad31
David Giard <djgiard@hotmail.com>
2016-08-12 07:37:44 -0500

    Added responsive column sizing

--- dgtvweb/app/app.component.ts
+++ dgtvweb/app/app.component.ts
@@ -6,18 +6,15 @@ import { TfHeaderComponent } from './tfHeader.component';
 
 
 @Component({
-    selector: 'my-app',
-    template: `
-      <tfHeader [title]='title' [subTitle]='subTitle' class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></tfHeader>
-      <router-outlet></router-outlet>
-    `,
+  selector: 'my-app',
+  templateUrl: '/app/app.html',
   directives: [ROUTER_DIRECTIVES, TfHeaderComponent],
-    providers: [
+  providers: [
     EpisodeService
   ]
 
 })
-export class AppComponent { 
+export class AppComponent {
   'title' = 'Technology and Friends';
   'subTitle' = 'Talking with smart people about technology';
 }
